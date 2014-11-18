5fbfc2ffad3a09ac45415ef923b5e4d6a230da83
B.Pirasanth <B.Pirasanth@10.10.13.154>
2014-11-18 15:54:54 -0600

    Separated UI Code in framework and BankFrm

--- src/main/java/banking/ui/BankFrm.java
+++ src/main/java/banking/ui/BankFrm.java
@@ -12,11 +12,8 @@ import framework.operation.SearchAccount;
 import framework.operation.SearchCondition;
 import framework.operation.Transaction;
 import framework.ui.MainUI;
-import java.awt.BorderLayout;
 import java.util.List;
 import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -30,150 +27,35 @@ public class BankFrm extends MainUI {
      */
     String accountnr, clientName, street, city, zip, state, accountType, clientType,
             amountDeposit, email, birthdate, numberofEmployees;
-    boolean newaccount;
-    private DefaultTableModel model;
-    private JTable JTable1;
-    private JScrollPane JScrollPane1;
     BankFrm myframe;
-    private Object rowdata[];
     BankUtil bankUtil;
     FinancialSystem financialSystem;
+    protected boolean newaccount;
 
     public BankFrm() {
         myframe = this;
         bankUtil = new BankUtil();
         financialSystem = new FinancialSystem(this);
 
-        setTitle("Bank Application.");
-        setDefaultCloseOperation(javax.swing.JFrame.DO_NOTHING_ON_CLOSE);
-        getContentPane().setLayout(new BorderLayout(0, 0));
-        setSize(575, 310);
-        setVisible(false);
-        JPanel1.setLayout(null);
-        getContentPane().add(BorderLayout.CENTER, JPanel1);
-        JPanel1.setBounds(0, 0, 575, 310);
-        /*
-         /Add five buttons on the pane 
-         /for Adding personal account, Adding company account
-         /Deposit, Withdraw and Exit from the system
-         */
-        JScrollPane1 = new JScrollPane();
-        model = getNewTableModel();
-        JTable1 = new JTable(model);
-        rowdata = new Object[8];
+        // rowdata = new Object[8];
         newaccount = false;
 
-        JPanel1.add(JScrollPane1);
-        JScrollPane1.setBounds(12, 92, 444, 160);
-        JScrollPane1.getViewport().add(JTable1);
-        JTable1.setBounds(0, 0, 420, 0);
-//        rowdata = new Object[8];
-
-        JButton_PerAC.setText("Add personal account");
-        JPanel1.add(JButton_PerAC);
-        JButton_PerAC.setBounds(24, 20, 192, 33);
-        JButton_CompAC.setText("Add company account");
-        JButton_CompAC.setActionCommand("jbutton");
-        JPanel1.add(JButton_CompAC);
-        JButton_CompAC.setBounds(240, 20, 192, 33);
-        JButton_Deposit.setText("Deposit");
-        JPanel1.add(JButton_Deposit);
-        JButton_Deposit.setBounds(468, 104, 96, 33);
-        JButton_Withdraw.setText("Withdraw");
-        JPanel1.add(JButton_Withdraw);
-        JButton_Addinterest.setBounds(448, 20, 106, 33);
-        JButton_Addinterest.setText("Add interest");
-        JPanel1.add(JButton_Addinterest);
-        JButton_Withdraw.setBounds(468, 164, 96, 33);
-        JButton_Exit.setText("Exit");
-        JPanel1.add(JButton_Exit);
-        JButton_Exit.setBounds(468, 248, 96, 31);
-        // lineBorder1.setRoundedCorners(true);
-        // lineBorder1.setLineColor(java.awt.Color.green);
-        //$$ lineBorder1.move(24,312);
-
-        JButton_PerAC.setActionCommand("jbutton");
-
-        SymWindow aSymWindow = new SymWindow();
-        this.addWindowListener(aSymWindow);
-        SymAction lSymAction = new SymAction();
-        JButton_Exit.addActionListener(lSymAction);
-        JButton_PerAC.addActionListener(lSymAction);
-        JButton_CompAC.addActionListener(lSymAction);
-        JButton_Deposit.addActionListener(lSymAction);
-        JButton_Withdraw.addActionListener(lSymAction);
-        JButton_Addinterest.addActionListener(lSymAction);
-
     }
 
-    javax.swing.JPanel JPanel1 = new javax.swing.JPanel();
-    javax.swing.JButton JButton_PerAC = new javax.swing.JButton();
-    javax.swing.JButton JButton_CompAC = new javax.swing.JButton();
-    javax.swing.JButton JButton_Deposit = new javax.swing.JButton();
-    javax.swing.JButton JButton_Withdraw = new javax.swing.JButton();
-    javax.swing.JButton JButton_Addinterest = new javax.swing.JButton();
-    javax.swing.JButton JButton_Exit = new javax.swing.JButton();
-
-    void exitApplication() {
-        try {
-            this.setVisible(false);    // hide the Frame
-            this.dispose();            // free the system resources
-            System.exit(0);            // close the application
-        } catch (Exception e) {
-        }
-    }
-
-    class SymWindow extends java.awt.event.WindowAdapter {
-
-        public void windowClosing(java.awt.event.WindowEvent event) {
-            Object object = event.getSource();
-            if (object == BankFrm.this) {
-                BankFrm_windowClosing(event);
-            }
-        }
-    }
-
-    void BankFrm_windowClosing(java.awt.event.WindowEvent event) {
-        // to do: code goes here.
-
-        BankFrm_windowClosing_Interaction1(event);
-    }
-
-    void BankFrm_windowClosing_Interaction1(java.awt.event.WindowEvent event) {
-        try {
-            this.exitApplication();
-        } catch (Exception e) {
-        }
-    }
-
-    class SymAction implements java.awt.event.ActionListener {
-
-        public void actionPerformed(java.awt.event.ActionEvent event) {
-            Object object = event.getSource();
-            if (object == JButton_Exit) {
-                JButtonExit_actionPerformed(event);
-            } else if (object == JButton_PerAC) {
-                JButtonPerAC_actionPerformed(event);
-            } else if (object == JButton_CompAC) {
-                JButtonCompAC_actionPerformed(event);
-            } else if (object == JButton_Deposit) {
-                JButtonDeposit_actionPerformed(event);
-            } else if (object == JButton_Withdraw) {
-                JButtonWithdraw_actionPerformed(event);
-            } else if (object == JButton_Addinterest) {
-                JButtonAddinterest_actionPerformed(event);
-            }
-
-        }
+    @Override
+    public String getTitle() {
+        return "Bank Application";
     }
 
     //When the Exit button is pressed this code gets executed
     //this will exit from the system
-    void JButtonExit_actionPerformed(java.awt.event.ActionEvent event) {
+    @Override
+    public void JButtonSix_actionPerformed(java.awt.event.ActionEvent event) {
         System.exit(0);
     }
 
-    void JButtonPerAC_actionPerformed(java.awt.event.ActionEvent event) {
+    @Override
+    public void JButtonOne_actionPerformed(java.awt.event.ActionEvent event) {
         /*
          JDialog_AddPAcc type object is for adding personal information
          construct a JDialog_AddPAcc type object 
@@ -198,7 +80,8 @@ public class BankFrm extends MainUI {
 
     }
 
-    void JButtonCompAC_actionPerformed(java.awt.event.ActionEvent event) {
+    @Override
+    public void JButtonTwo_actionPerformed(java.awt.event.ActionEvent event) {
         /*
          construct a JDialog_AddCompAcc type object 
          set the boundaries and 
@@ -223,9 +106,10 @@ public class BankFrm extends MainUI {
 
     }
 
-    void JButtonDeposit_actionPerformed(java.awt.event.ActionEvent event) {
+    @Override
+    public void JButtonThree_actionPerformed(java.awt.event.ActionEvent event) {
         // get selected name
-        int selection = JTable1.getSelectionModel().getMinSelectionIndex();
+        int selection = jTable.getSelectionModel().getMinSelectionIndex();
         if (selection >= 0) {
             String accnr = (String) model.getValueAt(selection, 0);
 
@@ -253,9 +137,10 @@ public class BankFrm extends MainUI {
 
     }
 
-    void JButtonWithdraw_actionPerformed(java.awt.event.ActionEvent event) {
+    @Override
+    public void JButtonFour_actionPerformed(java.awt.event.ActionEvent event) {
         // get selected name
-        int selection = JTable1.getSelectionModel().getMinSelectionIndex();
+        int selection = jTable.getSelectionModel().getMinSelectionIndex();
         if (selection >= 0) {
             String accnr = (String) model.getValueAt(selection, 0);
 
@@ -282,11 +167,12 @@ public class BankFrm extends MainUI {
 
     }
 
-    void JButtonAddinterest_actionPerformed(java.awt.event.ActionEvent event) {
+    @Override
+    public void JButtonFive_actionPerformed(java.awt.event.ActionEvent event) {
         Transaction transaction = bankUtil.getAddInterestCommand();
         financialSystem.doTransaction(transaction);
         updateTableModel();
-        JOptionPane.showMessageDialog(JButton_Addinterest, "Add interest to all accounts", "Add interest to all accounts", JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(JButton_Five, "Add interest to all accounts", "Add interest to all accounts", JOptionPane.WARNING_MESSAGE);
     }
 
     public void updateTableModel() {
@@ -305,8 +191,8 @@ public class BankFrm extends MainUI {
             rowdata[4] = account.getAccountType();
             rowdata[5] = account.getAmount();
             model.addRow(rowdata);
-            JTable1.setModel(model);
-            JTable1.getSelectionModel().setAnchorSelectionIndex(-1);
+            jTable.setModel(model);
+            jTable.getSelectionModel().setAnchorSelectionIndex(-1);
             newaccount = false;
         }
 
@@ -321,5 +207,46 @@ public class BankFrm extends MainUI {
         model.addColumn("Ch/S");
         model.addColumn("Amount");
         return model;
+    }
+
+    public void addButtonOne() {
+        JButton_One.setText("Add personal account");
+        mainPanel.add(JButton_One);
+        JButton_One.setBounds(24, 20, 192, 33);
+        JButton_One.setActionCommand("jbutton");
+    }
+
+    public void addButtonTwo() {
+        JButton_Two.setText("Add company account");
+        JButton_Two.setActionCommand("jbutton");
+
+        mainPanel.add(JButton_Two);
+        JButton_Two.setBounds(240, 20, 192, 33);
+    }
+
+    // Deposit
+    public void addButtonThree() {
+        JButton_Three.setText("Deposit");
+        mainPanel.add(JButton_Three);
+        JButton_Three.setBounds(468, 104, 96, 33);
+    }
+
+    // Withdraw
+    public void addButtonFour() {
+        JButton_Four.setText("Withdraw");
+        mainPanel.add(JButton_Four);
+        JButton_Four.setBounds(468, 164, 96, 33);
+    }
+
+    public void addButtonFive() {
+        JButton_Five.setBounds(448, 20, 106, 33);
+        JButton_Five.setText("Add interest");
+        mainPanel.add(JButton_Five);
+    }
+
+    public void addButtonSix() {
+        JButton_Six.setText("Exit");
+        mainPanel.add(JButton_Six);
+        JButton_Six.setBounds(468, 248, 96, 31);
     }
 }
--- src/main/java/framework/ui/MainUI.java
+++ src/main/java/framework/ui/MainUI.java
@@ -1,11 +1,179 @@
 package framework.ui;
 
+import banking.ui.BankFrm;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author malalanayake
  */
 public abstract class MainUI extends JFrame {
+
+    protected JPanel mainPanel;
+    protected JScrollPane jScrollPane;
+
+    protected JTable jTable;
+
+    protected DefaultTableModel model;
+
+    protected Object rowdata[];
+
+    protected JButton JButton_One;
+    protected JButton JButton_Two;
+    protected JButton JButton_Three;
+    protected JButton JButton_Four;
+    protected JButton JButton_Five;
+    protected JButton JButton_Six;
+
+    public MainUI() {
+        setTitle(getTitle());
+        setDefaultCloseOperation(javax.swing.JFrame.DO_NOTHING_ON_CLOSE);
+        getContentPane().setLayout(new BorderLayout(0, 0));
+        setSize(575, 310);
+        setVisible(false);
+
+        mainPanel = new javax.swing.JPanel();
+        mainPanel.setLayout(null);
+        getContentPane().add(BorderLayout.CENTER, mainPanel);
+        mainPanel.setBounds(0, 0, 575, 310);
+
+        /*
+         /Add five buttons on the pane 
+         /for Adding personal account, Adding company account
+         /Deposit, Withdraw and Exit from the system
+         */
+        jScrollPane = new JScrollPane();
+        model = getNewTableModel();
+        jTable = new JTable(model);
+        rowdata = new Object[8];
+
+        mainPanel.add(jScrollPane);
+        jScrollPane.setBounds(12, 92, 444, 160);
+        jScrollPane.getViewport().add(jTable);
+        jTable.setBounds(0, 0, 420, 0);
+
+        JButton_One = new JButton();
+        JButton_Two = new JButton();
+        JButton_Three = new JButton();
+        JButton_Four = new JButton();
+        JButton_Five = new JButton();
+        JButton_Six = new JButton();
+
+        this.addButtonOne();
+        this.addButtonTwo();
+        this.addButtonThree();
+        this.addButtonFour();
+        this.addButtonFive();
+        this.addButtonSix();
+
+        SymWindow aSymWindow = new SymWindow();
+        this.addWindowListener(aSymWindow);
+        SymAction lSymAction = new SymAction();
+        JButton_Six.addActionListener(lSymAction);
+        JButton_One.addActionListener(lSymAction);
+        JButton_Two.addActionListener(lSymAction);
+        JButton_Three.addActionListener(lSymAction);
+        JButton_Four.addActionListener(lSymAction);
+        JButton_Five.addActionListener(lSymAction);
+    }
+
+    private DefaultTableModel getNewTableModel() {
+        model = new DefaultTableModel();
+        model.addColumn("AccountNr");
+        model.addColumn("Name");
+        model.addColumn("City");
+        model.addColumn("P/C");
+        model.addColumn("Ch/S");
+        model.addColumn("Amount");
+        return model;
+    }
+
+    public abstract String getTitle();
+
+    public abstract void addButtonOne();
+
+    public abstract void addButtonTwo();
+
+    // Deposit
+    public abstract void addButtonThree();
+
+    // Withdraw
+    public abstract void addButtonFour();
+
+    public abstract void addButtonFive();
+
+    public abstract void addButtonSix();
+
+    class SymWindow extends java.awt.event.WindowAdapter {
+
+        public void windowClosing(java.awt.event.WindowEvent event) {
+            Object object = event.getSource();
+            if (object == this) {
+                BankFrm_windowClosing(event);
+            }
+        }
+    }
+
+    void BankFrm_windowClosing(java.awt.event.WindowEvent event) {
+		// to do: code goes here.
+
+        BankFrm_windowClosing_Interaction1(event);
+    }
+
+    void BankFrm_windowClosing_Interaction1(java.awt.event.WindowEvent event) {
+        try {
+            this.exitApplication();
+        } catch (Exception e) {
+        }
+    }
+
+    void exitApplication() {
+        try {
+            this.setVisible(false);    // hide the Frame
+            this.dispose();            // free the system resources
+            System.exit(0);            // close the application
+        } catch (Exception e) {
+        }
+    }
+
+    private class SymAction implements java.awt.event.ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+            Object object = event.getSource();
+            if (object == JButton_Six) {
+                JButtonSix_actionPerformed(event);
+            } else if (object == JButton_One) {
+                JButtonOne_actionPerformed(event);
+            } else if (object == JButton_Two) {
+                JButtonTwo_actionPerformed(event);
+            } else if (object == JButton_Three) {
+                JButtonThree_actionPerformed(event);
+            } else if (object == JButton_Four) {
+                JButtonFour_actionPerformed(event);
+            } else if (object == JButton_Five) {
+                JButtonFive_actionPerformed(event);
+            }
+
+        }
+    }
+
+    public abstract void JButtonOne_actionPerformed(ActionEvent event);
+
+    public abstract void JButtonTwo_actionPerformed(ActionEvent event);
+
+    public abstract void JButtonThree_actionPerformed(ActionEvent event);
+
+    public abstract void JButtonFour_actionPerformed(ActionEvent event);
+
+    public abstract void JButtonFive_actionPerformed(ActionEvent event);
+
+    public abstract void JButtonSix_actionPerformed(ActionEvent event);
 
 }
